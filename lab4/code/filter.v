328d3fe8567bd9ab7dff226f58e1811fdeb6b897
Marcel Moreaux <marcel@moreaux.nl>
2014-06-14 22:46:19 +0200

    Improved comments of filter.v

--- lab4/code/filter.v
+++ lab4/code/filter.v
@@ -25,20 +25,25 @@ module filter
 	reg req_in_buf;
 	assign req_in = req_in_buf;
 
+	// state counter. l = nM mod L (calculated efficiently using conditionals and addition/subtraction)
 	reg [0:L_LOG-1] l;
+	// The last 4 input samples used in the FIR
 	reg signed [0:DWIDTH-1] in [0:3];
+	// The FIR coefficients (lots of them)
 	reg signed [0:DWIDTH-1] h [0:4*L-1];
 	
-	// Accumulator (assigned to output directly)
+	// The 4 products in the sum are buffered as partial results for pipelining
 	reg signed [0:DDWIDTH-1] partial1;
 	reg signed [0:DDWIDTH-1] partial2;
 	reg signed [0:DDWIDTH-1] partial3;
 	reg signed [0:DDWIDTH-1] partial4;
+	// Accumulator (lower bits assigned to output port directly)
 	reg signed [0:DDWIDTH-1] sum;
 	assign data_out = sum >> 15;
 
 	initial
 	begin
+		// Initialize the ROM with coefficients from file
 		$readmemh("coefficients.txt", h);
 	end
 
@@ -55,36 +60,45 @@ module filter
 		// !Reset => run
 		else
 		begin
-			// Input request & acknowledge => take the input & go back to computation a.s.a.p.
+			// Input
 			if (req_in && ack_in)
 			begin
+				// Read new input sample, and shift older samples
 				in[0] <= data_in;
 				in[1] <= in[0];
 				in[2] <= in[1];
 				in[3] <= in[2];
+				// Input is done
 				req_in_buf <= 0;
 			end
-			// Output request & acknowledge => go back to computation a.s.a.p.
+
+			// Output
 			if (req_out && ack_out)
 			begin
+				// Output is done
 				req_out_buf <= 0;
 			end
-			// If we need no inputs and have no outputs ready, then proceed with the computation
+
+			// No I/O, computation cycle
 			if (!req_in && !req_out && !ack_in && !ack_out)
 			begin
+				// l <= (l + M) mod L, implemented with conditionals
+				// No-overflow case
 				if( l < L - M )
 				begin
 					l <= l + M;
 				end
+				// Overflow case. Overflow also means we need a new input!
 				else
 				begin
 					l <= l - (L - M);
 					req_in_buf <= 1;
 				end
 
+				// We output after every computation cycle
 				req_out_buf <= 1;
 
-				// sum <= (in[0] * h[l] + in[1] * h[l+L] + in[2] * h[l+L*2] + in[3] * h[l+L*3]);
+				// pipelined: sum <= (in[0] * h[l] + in[1] * h[l+L] + in[2] * h[l+L*2] + in[3] * h[l+L*3]);
 				partial1 <= in[0] * h[l];
 				partial2 <= in[1] * h[l+L];
 				partial3 <= in[2] * h[l+L*2];
